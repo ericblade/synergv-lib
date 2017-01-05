a9a36e83a4b98a3b8ed57ff2fb7ab821751a34ea
Eric Blade <blade.eric@gmail.com>
2017-01-05 01:40:16 -0500

    improvements to testsuite
    eliminate most eslint errors
    make functions out of all the tests that vary only slightly
    all tests after the initial setup are down to a single line of code,
    making the entire test pretty easy to read, at least if you're used
    to staring at ES6 javascript all day :-)

--- samples/testsuite.js
+++ samples/testsuite.js
@@ -1,4 +1,6 @@
-const VERSION = '0.0.8';
+/* eslint no-console: "off" */
+
+const VERSION = '0.0.9';
 
 const login = require('./login');
 const getBillingCredit = require('..').getBillingCredit;
@@ -39,71 +41,84 @@ function testTokens(args) {
 }
 
 function sendTestMessage() {
-    console.log(`**** sending test message from ${gcData.number.raw} to self for further testing.`)
+    console.log(`**** sending test message from ${gcData.number.raw} to self for further testing.`);
     return sendMessage({ recp: gcData.number.raw, text: 'TEST SUITE MESSAGE' });
 }
 
-function testResultIsOk(result) {
+function testResultIsOk(result, echoResult = false) {
     if (result.ok !== true) {
         throw new Error('Failed. err=', result);
     } else {
         console.warn('**** Result OK: true');
+        if (echoResult) {
+            console.warn('**** Result was:', result);
+        }
         return true;
     }
 }
 
-function retrieveTestMessageFromInbox() {
-    console.warn('**** Searching for test messages');
+function testLabelNotPresent(conv, label) {
+    if (conv.labels.indexOf(label) > -1) {
+        throw new Error(`Expected conversation to not have label ${label}`)
+    }
+    console.warn(`**** Conversation does not contain label ${label} as expected`);
+    return true;
+}
+
+function testConversationValue(conv, name, compareValue) {
+    if (conv[name] !== compareValue) {
+        throw new Error(`Expected variable ${name} to contain ${compareValue}, but it is ${conv[name]}`);
+    }
+    console.warn(`**** Conversation variable ${name} contains ${compareValue} as expected`);
+}
+
+function retrieveTestMessageFromBox(box = 'inbox') {
+    console.warn(`**** Searching for test messages in box ${box}`);
     return new Promise((resolve, reject) => {
-        getBox({ label: 'inbox', p: 1 })
+        getBox({ label: box, p: 1 })
         .then((conversations) => {
-            const results = [];
-            for (c in conversations) {
-                const conv = conversations[c];
-                if (conv.messageText === 'TEST SUITE MESSAGE' && conv.phoneNumber === gcData.number.raw) {
-                    results.push(conv);
-                }
-            }
+            const results = conversations.filter(c => c.messageText === 'TEST SUITE MESSAGE' && c.phoneNumber === gcData.number.raw);
             resolve(results);
         }).catch((err) => {
             throw new Error({ error: 'Error retrieving inbox?!', err });
         });
-    })
+    });
 }
 
-function checkTestMessageCount(count) {
-    return retrieveTestMessageFromInbox()
+function checkTestMessageCount(count, box = 'inbox') {
+    return retrieveTestMessageFromBox(box)
         .then((convs) => {
             if (convs.length === count) {
-                console.warn(`**** inbox test message count is expected ${count}`);
+                console.warn(`**** ${box} test message count is expected ${count}`);
                 return convs;
-            } else {
-                throw new Error(`Expected ${count} test message conversation, found ${convs.length}`);
-                return false;
             }
+            throw new Error(`Expected ${count} test message conversation, found ${convs.length}`);
         });
+}
+
+function header(str) {
+    console.warn(`**** ${str}`);
+    return false;
+}
+
+function wait(time) {
+    return new Promise((resolve) => setTimeout(() => resolve(true), time));
 }
 
 console.log('**** Logging in to retrieve tokens');
 
 login.login()
 .then((...args) => testTokens(args))
-.then(() => {
-    console.warn('**** Testing getBillingCredit');
-    return getBillingCredit();
-})
-.then((billingCredit) => {
-    testResultIsOk(billingCredit);
-    console.warn('**** billingCredit: ', billingCredit);
-})
-.then(() => retrieveTestMessageFromInbox(gcData.number.raw)) // check for existing test messages
+.then(() => header('getBillingCredit') || getBillingCredit())
+.then(resp => testResultIsOk(resp, true))
+.then(() => retrieveTestMessageFromBox()) // check for existing test messages
 .then((existingTests) => {
     if (existingTests.length) { // delete them permanently if found
         console.warn('**** Deleting existing test messages from inbox', existingTests);
-        const existingTestIds = existingTests.map((conv) => conv.id);
+        const existingTestIds = existingTests.map(conv => conv.id);
         const promise = deleteForever(existingTestIds, true)
-            .then(retrieveTestMessageFromInbox(gcData.number.raw)) // check that delete worked
-            .then(stillExistingTests => {
+            .then(retrieveTestMessageFromBox()) // check that delete worked
+            .then((stillExistingTests) => {
                 if (stillExistingTests.length) {
                     throw new Error('Test Messages still existing even after delete!');
                 } else {
@@ -112,74 +127,54 @@ login.login()
                 return true;
             });
         return promise;
-    } else {
-        console.warn('**** No existing test messages found');
-        return true;
     }
+    console.warn('**** No existing test messages found');
+    return true;
 })
 .then(() => sendTestMessage()) // test send message function
-.then((resp) => testResultIsOk(resp))
-.then(() => {
-    console.warn('**** Waiting 3 seconds for message to appear in Inbox');
-    return new Promise( (resolve, reject) => {
-        setTimeout(() => resolve(checkTestMessageCount(1)), 3000);
-    });
-})
+.then(resp => testResultIsOk(resp, true))
+.then(() => header('Waiting 3 seconds for message to appear in Inbox') || wait(3000))
+.then(() => checkTestMessageCount(1))
 .then((testIds) => {
     console.warn(`**** New Test Message Conversation Id: ${testIds[0]}`);
     testId = testIds[0].id;
     return true;
 })
 
-.then(() => { // test Archive
-    console.warn('**** Archiving');
-    return archiveMessages([testId], true);
-})
-.then((resp) => testResultIsOk(resp))
-.then(() => checkTestMessageCount(0)) // TODO: This should be modded to check the "All" box for 1 test message, and verify it doesn't have the Inbox folder set.
-.then(() => { // test Unarchive
-    console.warn('**** Unarchiving');
-    return archiveMessages([testId], false);
-})
-.then((resp) => testResultIsOk(resp))
+.then(() => header('Archiving') || archiveMessages([testId], true))
+.then(resp => testResultIsOk(resp))
+.then(() => checkTestMessageCount(1, 'all'))
+.then((convs) => testLabelNotPresent(convs[0], 'Inbox'))
+.then(() => header('Unarchiving') || archiveMessages([testId], false))
+.then(resp => testResultIsOk(resp))
 .then(() => checkTestMessageCount(1))
 
-.then(() => { // test Block
-    console.warn('**** Blocking');
-    return blockMessage([testId], true);
-})
-.then((resp) => testResultIsOk(resp))
-.then(() => checkTestMessageCount(0)) // TODO: This should be modded to check the 'Spam' box for 1 test message, and check it's isSpam attribute
-.then(() => { // test Unblock
-    console.warn('**** Unblocking');
-    return blockMessage([testId], false);
-})
-.then((resp) => testResultIsOk(resp))
+.then(() => header('Blocking') || blockMessage([testId], true))
+.then(resp => testResultIsOk(resp))
+.then(() => checkTestMessageCount(1, 'spam'))
+.then(convs => testConversationValue(convs[0], 'isSpam', true))
+.then(() => header('Unblocking') || blockMessage([testId], false))
+.then(resp => testResultIsOk(resp))
 .then(() => checkTestMessageCount(1))
+.then(convs => testConversationValue(convs[0], 'isSpam', false))
 
-.then(() => { // test Delete
-    console.warn('**** Deleting');
-    return deleteMessage([testId], true);
-})
-.then((resp) => testResultIsOk(resp))
-.then(() => checkTestMessageCount(0)) // TODO: This should be modded to check the 'Trash' box for 1 test message and check it's isTrash attribute
-.then(() => { // test Undelete
-    console.warn('**** Undeleting');
-    return deleteMessage([testId], false);
-})
-.then((resp) => testResultIsOk(resp))
+.then(() => header('Delete') || deleteMessage([testId], true))
+.then(resp => testResultIsOk(resp))
+.then(() => checkTestMessageCount(1, 'trash'))
+.then(convs => testConversationValue(convs[0], 'isTrash', true))
+.then(() => header('Undelete') || deleteMessage([testId], false))
+.then(resp => testResultIsOk(resp))
 .then(() => checkTestMessageCount(1))
+.then(convs => testConversationValue(convs[0], 'isTrash', false))
 
-.then(() => { // test Mark (read)
-    console.warn('**** Marking Read');
-    return markRead([testId], true);
-})
-.then((resp) => testResultIsOk(resp)) // TODO: Need to add a step after this that checks the "isRead" status on the message.
-.then(() => { // test UnMark (unread)
-    console.warn('**** Marking Unread');
-    return markRead([testId], false);
-})
-.then((resp) => testResultIsOk(resp)) // TODO: Need to add a step after this that checks the "isRead" status on the message.
+.then(() => header('Mark Read') || markRead([testId], true))
+.then(resp => testResultIsOk(resp))
+.then(() => checkTestMessageCount(1))
+.then(convs => testConversationValue(convs[0], 'isRead', true))
+.then(() => header('Mark UNread') || markRead([testId], false))
+.then(resp => testResultIsOk(resp))
+.then(() => checkTestMessageCount(1))
+.then(convs => testConversationValue(convs[0], 'isRead', false))
 
 // how to test searchMessages? probably provide a search query for the test message, and validate that it ONLY has that message.
 // TODO: Now that I'm thinking about it, we should do a searchMessages on that at the very top instead of a Inbox get,
